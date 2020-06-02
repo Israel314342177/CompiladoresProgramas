0fc748c16cd88000f53e336d8c0a398e07ecc98c
Eduardo-MMz <54245711+Eduardo-MMz@users.noreply.github.com>
2020-06-02 15:46:56 -0500

    Update tabla_simbolos.h

--- Programa3/tabla_simbolos.h
+++ Programa3/tabla_simbolos.h
@@ -7,7 +7,7 @@ void append_sym(SYMTAB *SS, SYM *S); //Agrega al final de la tabla un nuevo simb
 void clear_sym_tab(SYMTAB *SS); // Deja vacia la tabla
 
 SYMTAB *pop_st(SSTACK *SSS); // Ejecuta un pop sobre la pila de tablas de simbolos
-void push_st (SSTACK *SSS, SYMTAB *SS); // Ingresa una tabla a la pila de tablas de simbolos
+void push_st(SSTACK *SSS, SYMTAB *SS); // Ingresa una tabla a la pila de tablas de simbolos
 SSTACK *init_sym_tab_stack(); // Reserva memoria para la pila
 SYMTAB *init_sym_tab(); // Reserva memoria para una tabla de simbolos vacia
 SYM *init_sym(); // Reserva memoria para un simbolo vacio
@@ -15,6 +15,6 @@ SYM *init_sym(); // Reserva memoria para un simbolo vacio
 void finish_sym_tab_stack(SSTACK *SSS); // Libera la memoria para la pila
 void finish_sym_tab(SYMTAB *SS); // Libera memoria para una tabla de simbolos
 void finish_sym(SYM *S); // libera memoria para un simbolo vacio
-void print_tab(SYMTAB *SS); // Imprime en pantalla la tabla de simbolos
+void print_Stab(SYMTAB *SS); // Imprime en pantalla la tabla de simbolos
 
 #endif
