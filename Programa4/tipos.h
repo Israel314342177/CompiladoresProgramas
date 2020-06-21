d3eebf9f27dde874f1265d2f495844ef31a2df83
Eduardo-MMz <54245711+Eduardo-MMz@users.noreply.github.com>
2020-06-20 19:17:48 -0500

    Update tipos.h

--- Programa4/tipos.h
+++ Programa4/tipos.h
@@ -1,6 +1,7 @@
 #ifndef tipos_H
 #define tipos_H
 #include "cuadruplas.h"
+#include "tabla_tipos.h"
 /*
 Recibe dos tipos, cada uno es su indice hacia la tabla de tipos en la cima de la pila
 Si el primero es mas chico que el segundo genera la cuadrupla
@@ -8,23 +9,30 @@ para realizar la ampliacion del tipo.
 Retorna la nueva direccion generada por la ampliacion
 o la direccion original en caso de que no se realice
 */
-char *ampliar(char *dir, int t1, int t2, CODE *c);
+char *ampliar(char *dir, int t1, int t2, CODE *c,TSTACK *Tpila);
 
 
-char *reducir(char *dir, int t1, int t2, CODE *c);
+char *reducir(char *dir, int t1, int t2, CODE *c,TSTACK *Tpila);
 
 
 /*
 Recibe dos tipos, cada uno es indice hacia la tabla de tipos en la cima de la pila
 retorna el indice de mayor dimension
 */
-int max(int t1, int t2);
+int max(int t1, int t2,TSTACK *Tpila);
 
 
 /*
 Recibe dos tipos, cada uno es indice hacia la tabla de tipos en la cima de la pila
 retorna el indice de menor dimension
 */
-int min(int t1, int t2);
+int min(int t1, int t2,TSTACK *Tpila);
+
+
+/*
+Recibe dos tipos, cada uno es indice hacia la tabla de tipos en la cima de la pila
+retorna el indice de menor dimension si minMax es 0, o retorna el indice de mayor dimension si minMax es 1
+*/
+int buscaTamTipos(int t1, int t2,TYPTAB *top,int minMax);
 
 #endif
